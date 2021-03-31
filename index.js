6fa15206a58b00b36430f27e5c3ee2c582de4964
fred441a <frederik@altofte.dk>
2021-03-31 12:02:45 +0200

    iw

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+var hostapd = require('wireless-tools/hostapd');
+var wpa_cli = require('wireless-tools/wpa_cli');
+var udhcpd = require('wireless-tools/udhcpd');
+var wpa_supplicant = require('wireless-tools/wpa_supplicant');
+
 const express = require('express')
-var wifi = require('node-wifi');
 const bp = require('body-parser')
 fs = require('fs');
 const app = express()
 const port = 3000
+
+
 
 var discord = require("./DiscordBot");
 
@@ -12,23 +18,21 @@ app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 
-wifi.init({
-    iface: null // network interface, choose a random wifi interface if set to null
-});
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/config/index.html')
 })
 
 app.get('/WifiScan', (req, res) => {
-    wifi.scan((error, network) => {
-        if (error) {
-            console.error(error);
-        } else {
-            res.send(network);
-        }
+
+    wpa_cli.scan('wlan0', function(err, data){
+        wpa_cli.scan_results('wlan0', function(err, data) {
+           // returns the results of the BSS scan once it completes
+           console.log(data);
+           res.send(data)
+        })
     });
-})
+
+});
 
 app.post('/Setup', (req, res) => {
     let ENV = "DiscordBotKey = "+req.body.BotToken+" \nChannel = "+req.body.Channel;
@@ -38,15 +42,64 @@ app.post('/Setup', (req, res) => {
         }
     });
     console.log(req.body);
-    wifi.connect({ ssid: req.body.SSID, password: req.body.WifiPassword }, error => {
-        if (error) {
-            console.log(error);
-        }
-        console.log('Connected');
-    });
-    discord.SetupBot();
+    var options = {
+        interface: 'wlan0',
+        ssid: req.body.SSID,
+        passphrase: req.body.WifiPassword,
+        driver: 'wext'
+      };
+       
+      wpa_supplicant.enable(options, function(err) {
+          console.log("Connected!")
+          discord.SetupBot();
+      });
+
+
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+function StartHotspot(){
+
+    var options = {
+        channel: 6,
+        driver: 'rtl871xdrv',
+        hw_mode: 'g',
+        interface: 'wlan0',
+        ssid: 'MemeFrame',
+        wpa: 2,
+        wpa_passphrase: 'MemeFrame'
+      };
+       
+      hostapd.enable(options, function(err) {
+        // the access point was created
+      });
+
+    var options = {
+        interface: 'wlan0',
+        start: '192.168.10.100',
+        end: '192.168.10.200',
+        option: {
+          router: '192.168.10.1',
+          subnet: '255.255.255.0',
+          dns: [ '4.4.4.4', '8.8.8.8' ]
+        }
+      };
+       
+      udhcpd.enable(options, function(err) {
+        // the dhcp server was started
+      });
+}
+
+function StopHotspot(){
+
+    hostapd.disable('wlan0', function(err) {
+        // no longer hosting the access point
+      });
+
+      udhcpd.disable('wlan0', function(err) {
+        // the dhcp server was stopped
+      });
+}
